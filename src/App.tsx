bd4511fe2ac928b623780c1204a71966c7ffbcee
boevoivertolet <bjha78rus@yandex.ru>
2023-03-31 10:31:02 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 function App() {
+    const [likes, setLikes] = useState(0)
+    const [value, setValue] = useState('Input text')
+
+    const increment = () => {
+        setLikes(likes + 1)
+    }
+    const decrement = () => {
+        setLikes(likes - 1)
+    }
+
+
     return (
         <div className = "App">
-
+            <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input type = "text" value = {value} onChange = {event => setValue(event.target.value)} />
+            <button onClick = {increment}>Increment</button>
+            <button onClick = {decrement}>Decrement</button>
         </div>
     );
 }
